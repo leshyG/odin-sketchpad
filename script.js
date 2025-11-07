7bba15b8ca6a06a812db3fedae8be11418beebcb
Leonardo <106412608+leshyG@users.noreply.github.com>
2025-11-06 22:54:00 -0500

    Añadido límite de cuadrícula en un prompt y verifica que esté entre 1 a 100 como máximo. Ahora en cada interacción el color de fondo cambia.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let createGrid = document.querySelector(".btn-grid");
 let grid = document.querySelector(".grid");
 createGrid.addEventListener("click", () => {
+    let size;
+    while (typeof (size) !== "number" || (0 > size || size > 100)) {
+        size = parseInt(prompt("¿Cuanto tamaño del sketch? (max: 100)"));
+    }
+    if (grid.hasChildNodes()) {
+        grid.textContent = "";
+    }
     grid.style = " width: 1500px;height: 725px;display: flex;flex-direction: column";
-    for (let i = 1; i <= 16; i++) {
+    for (let i = 1; i <= size; i++) {
         const column = document.createElement("div");
         column.className = `column`
         column.style = "display: flex; height: 100%";
-        for (let j = 1; j <= 16; j++) {
+        for (let j = 1; j <= size; j++) {
             const row = document.createElement("div");
             row.className = `row`;
             row.style = "flex: 1; border: 1px solid black";
@@ -14,8 +21,11 @@ createGrid.addEventListener("click", () => {
         }
         grid.appendChild(column);
         let divGrid = document.querySelectorAll(".row");
+        let ran1 = Math.floor((Math.random() * 255) + 1);
+        let ran2 = Math.floor((Math.random() * 255) + 1);
+        let ran3 = Math.floor((Math.random() * 255) + 1);
         divGrid.forEach((e) => e.addEventListener("mouseover", () => {
-            e.style.backgroundColor = "gray";
+            e.style.backgroundColor = `rgb(${ran1},${ran2},${ran3})`;
         }));
     }
 });
